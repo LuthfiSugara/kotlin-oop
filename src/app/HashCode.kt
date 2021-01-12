d9f4d8a8bb05ba1d2794a7d26daa851e4150e7bb
luthfi <sugaraluthfi@gmail.com>
2021-01-12 22:07:08 +0700

    test

--- src/app/HashCode.kt
+++ src/app/HashCode.kt
@@ -3,8 +3,8 @@ package app
 import data.Company
 
 fun main() {
-    val company1 = Company("Luthfi")
-    val company2 = Company("Luthfi")
+    val company1 = Company("Luthfi ")
+    val company2 = Company("Luthfi ")
 
     println(company1.hashCode() == company2.hashCode())
 
