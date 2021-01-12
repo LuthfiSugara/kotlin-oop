7139e247a05e0841443089eb997f6247a8e21e94
= <=>
2021-01-12 22:02:58 +0700

    test

--- src/app/HashCode.kt
+++ src/app/HashCode.kt
@@ -4,7 +4,7 @@ import data.Company
 
 fun main() {
     val company1 = Company("Luthfi")
-    val company2 = Company("Luthfi ")
+    val company2 = Company("Luthfi")
 
     println(company1.hashCode() == company2.hashCode())
 
